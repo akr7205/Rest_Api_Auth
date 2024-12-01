231be2d6f5c40e3738921691cf52d1fa735c4477
Arun Rout <you@example.com>
2024-12-01 22:37:50 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  accessTokenSecret: "myAccessTokenSecret",
+  accessTokenExpiresIn: "5m",
+
+  refreshTokenSecret: "myRefreshTokenSecret",
+  refreshTokenExpiresIn: "30m",
+
+  cacheTemporaryTokenPrefix: "temp_token:",
+  cacheTemporaryTokenExpiresInSeconds: 180,
+};
